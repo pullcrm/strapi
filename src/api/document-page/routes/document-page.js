bfabbfc39e62c7d6755d15388ec9068ba0b4bd30
tsaryk <tsaryk@uteka.ru>
2022-06-11 18:34:58 +0300

    WIP

---
+++ src/api/document-page/routes/document-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * document-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::document-page.document-page');
---
+++ src/api/document-page/services/document-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * document-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::document-page.document-page');
