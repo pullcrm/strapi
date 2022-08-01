3e5d6e696f855e66e216a96f7d80ecdd3068f495
b_tsaryk <b.tsaryk@keepsolid.com>
2022-08-01 14:35:04 +0300

    WIP

---
+++ src/api/articles-category/controllers/articles-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  articles-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::articles-category.articles-category');
